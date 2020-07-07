3cb8ecad8919b22ddb15d14d5905abf24b6b88ca
sathiyat <67980522+sathiyat@users.noreply.github.com>
2020-07-07 16:52:29 -0400

    Add files via upload

--- src/main/java/com/example/demo/DemoEurekaClient1Application.java
+++ src/main/java/com/example/demo/DemoEurekaClient1Application.java
@@ -9,9 +9,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class DemoEurekaClient1Application {
 
